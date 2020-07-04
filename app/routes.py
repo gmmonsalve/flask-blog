997c489c51b6e8525c375b4ce1b16a34bd9f12f4
gmmosnalve <gmmonsalve@uninorte.edu.co>
2020-07-03 19:08:32 -0500

    login route and methods

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,19 @@
-from flask import render_template
+from flask import render_template, redirect, flash, url_for
 from app import app
+from app.forms import LoginForm
 
-@app.route('/')
+@app.route('/index')
 def index():
-    user = {'username': 'Gabriela'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html',title='Welcome',posts=posts,user=user) 
+    return render_template('home.html') 
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f"Login requested for user {form.username.data}, remember_me={form.remember_me.data}")
+        return redirect(url_for('index'))
+    return render_template('login.html',form=form)
+
+@app.route('/register')
+def register():
+    return render_template('login.html')
