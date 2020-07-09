e9bbd83664b18485540dda1bd6d8c30f49a1503f
gmmosnalve <gmmonsalve@uninorte.edu.co>
2020-07-08 20:23:16 -0500

    Logging Users

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,30 @@
 from flask import render_template, redirect, flash, url_for
 from app import app
 from app.forms import LoginForm
+from app.models import User
+from flask_login import current_user, login_user 
 
 @app.route('/')
 def index():
     return redirect(url_for('login'))
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
 @app.route('/login', methods=['GET','POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f"Login requested for user {form.username.data}, remember_me={form.remember_me.data}")
-        return redirect(url_for('index'))
-    return render_template('login.html',form=form)
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid usename or password')
+            return redirect(url_for('login'))
+        login_user(user,remember=form.remember_me.data)
+        return redirect(url_for('home'))
+    return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/register')
 def register():
